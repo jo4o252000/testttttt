5a31a0d2cadcbff14cc25f29c793cf72750f5fa8
joaovictor25 <joao.custodio@55pbx.com>
2025-03-12 15:15:41 -0300

    .

--- src/routes/submission.js
+++ src/routes/submission.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.post("/api/submission", async (req, res) => {
     try {
-        
+        res.status(200).send("CHEGOU AQUI CARAI")
     }catch (error) {
         res.status(400).send(error)
     }
